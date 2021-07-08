dc75ba8b8ac4aff1ad578c53ed36b470a84fde71
Kirill Lavrov <lkv@myttk.ru>
2021-07-08 15:19:24 +0700

    fix: last edits

--- js/bookmark.js
+++ js/bookmark.js
@@ -2,6 +2,7 @@ const bookmarksList = document.querySelectorAll('.bookmark')
 
 bookmarksList.forEach((bookmark) => {
   bookmark.addEventListener('click', (event) => {
+    event.preventDefault()
     childOf(event.target, 'bookmark').classList.toggle('checked')
   })
 })
